60bb85dfc7eba63004ec0de2706c082ce32b4440
Abdullah Al Faiyaz <abdullah-al.faiyaz@stud.uni-bamberg.de>
2024-10-07 22:48:55 +0200

    useNavaitae and error page handled

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,16 +1,24 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 const Post = ({ post }) => {
   const { userId, id, title, body } = post;
+  const navigate = useNavigate();
   const postStyle = {
     border: "2px solid green",
     padding: "5px",
     borderRadius: "20px",
+  };
+  const handleShowDetail = () => {
+    navigate(`/post/${id}`);
   };
   return (
     <div style={postStyle}>
       <h4>Post of Id: {id}</h4>
       <p>{title}</p>
       <Link to={`/post/${id}`}>Post Details</Link>
+      <Link to={`/post/${id}`}>
+        <button>Show Details</button>
+      </Link>
+      <button onClick={handleShowDetail}>Click to see details</button>
     </div>
   );
 };
